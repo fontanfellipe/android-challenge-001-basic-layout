0226a1171f53c53e73f0637d165be5a0d8787b96
Peter Krause <krausepeterm@gmail.com>
2019-05-17 13:27:04 -0300

    Fix pull request comments

--- app/src/main/java/com/jungledevsbasiclayoutchallenge/BaseActivity.kt
+++ app/src/main/java/com/jungledevsbasiclayoutchallenge/BaseActivity.kt
@@ -8,7 +8,6 @@ open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
